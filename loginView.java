d67493a13d64b6b53a198e64b1bb0738a58e4a3a
Umur Ak <umurduhaak@gmail.com>
2016-02-05 09:44:14 +0100

    Small optimizations
    Class loginView showed warnings, took measurements to sort them out.

--- loginView.java
+++ loginView.java
@@ -1,6 +1,4 @@
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
@@ -18,6 +16,7 @@ import java.awt.event.ActionEvent;
 
 public class loginView extends JDialog {
 
+	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
 	private JTextField textFieldEmail;
 	private JPasswordField textFieldPassword;
